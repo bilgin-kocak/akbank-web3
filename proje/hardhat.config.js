b7689165e816f3acd4040e8235e34db0b00ac021
Bilgin <kocakbilgin@gmail.com>
2022-09-27 15:17:03 +0300

    deployment script added

--- proje/hardhat.config.js
+++ proje/hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
+  networks: {
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com/',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    polygon: {
+      url: 'https://polygon-rpc.com/',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
---
+++ proje/scripts/deploy.js
@@ -0,0 +1,52 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require('hardhat');
+
+async function main() {
+  const CourseRegistrar = await hre.ethers.getContractFactory(
+    'CourseRegistrar'
+  );
+  courseRegistrar = await CourseRegistrar.deploy();
+  await courseRegistrar.deployed();
+  console.log('CourseRegistrar deployed to:', courseRegistrar.address);
+
+  // Deploy ERC20 CMON token
+  const CourseMoney = await hre.ethers.getContractFactory('CourseMoney');
+  courseMoney = await CourseMoney.deploy();
+  await courseMoney.deployed();
+  console.log('CourseMoney deployed to:', courseMoney.address);
+
+  // Deploy Course Contract
+  const Course1 = await hre.ethers.getContractFactory('Course');
+  course1 = await Course1.deploy(
+    'Avalanche Course',
+    courseMoney.address,
+    ethers.utils.parseEther('100'),
+    2,
+    courseRegistrar.address
+  );
+  await course1.deployed();
+  console.log('Course1 deployed to:', course1.address);
+
+  const Course2 = await hre.ethers.getContractFactory('Course');
+  course2 = await Course2.deploy(
+    'Avalanche Course',
+    courseMoney.address,
+    ethers.utils.parseEther('100'),
+    2,
+    courseRegistrar.address
+  );
+  await course2.deployed();
+  console.log('Course2 deployed to:', course2.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
